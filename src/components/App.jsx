01418d66f49912b7062a8dd24e0dad5c8b9141f8
Anton Shabalov <shabalovanton@gmail.com>
2023-01-12 11:15:34 +0200

    done Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
+import {Statistics} from './Statistics/Statistics';
 import user from './Profile/user.json';
 
 export const App = () => {
   return (
     <>
       <Profile user = {user}/>
+      <Statistics/>
     </>
   );
 };
