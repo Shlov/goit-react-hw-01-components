9530d7e34f387084daab27896a79c3bcf78cdc31
Anton Shabalov <shabalovanton@gmail.com>
2023-01-14 21:31:44 +0200

    done hw 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,7 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { Friends } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from './Profile/user.json';
-import statistics from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
+import {user, statistics, friends, transactions} from './data';
 
 export const App = () => {
   return (
---
+++ src/components/data.js
@@ -0,0 +1,6 @@
+import user from './Profile/user.json';
+import statistics from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+
+export {user, statistics, friends, transactions};
